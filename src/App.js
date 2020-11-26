6a59e6beebc6c57b54d001e3221260ba630eadb4
madhu179 <madhubabu232343@gmail.com>
2020-11-26 16:31:41 +0530

    Adding code to use event binding technique

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import logo from './assets/logo.jpg';
 
 class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
   constructor(){
     super()
     this.state = {
       title: 'hello world from Bridgelabz'
     }
   }
+
+  onClick = ($event) => {
+    console.log("save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} 
+        <img src={logo} onClick={this.onClick} 
         alt="The Bridgelabz logo : a Bridge to Employment through lab works" />
       </div>
     );
