a800d65a2fbd6f936630ef6183c502981d61cada
madhu179 <madhubabu232343@gmail.com>
2020-11-26 16:36:05 +0530

    Adding code to add onchange event on an input element to display in text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      title: 'hello world from Bridgelabz'
+      userName: ''
     }
   }
 
@@ -15,15 +15,27 @@ class App extends React.Component {
     console.log("save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+
+  onNameChange = (event) => {
+    console.log("value is", event.target.value);
+    this.setState({userName: event.target.value})
+  }
+
   render() {
     return (
+      <>
       <div>
-        <h1>{this.state.title}</h1>
+        <h1>Hello {this.state.userName} from Bridgelabz</h1>
         <img src={logo} onClick={this.onClick} 
         alt="The Bridgelabz logo : a Bridge to Employment through lab works" />
       </div>
+      <div>
+        <input class="inputtext" onChange={this.onNameChange} />
+      </div>
+      </>
     );
   }
 }
+
 
 export default App;
